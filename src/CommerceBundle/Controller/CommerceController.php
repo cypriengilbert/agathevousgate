4500b5a9b1c841de7b02847eaa59eb86a0bcf18d
Cyprien Gilbert <apple@Apples-iMac.local>
2016-06-12 14:23:35 +0200

    add framwork front Foundation

---
+++ src/CommerceBundle/Controller/CommerceController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CommerceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class CommerceController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+
+
+            return $this->render('CommerceBundle:Default:index.html.twig');
+
+
+
+}
+}
