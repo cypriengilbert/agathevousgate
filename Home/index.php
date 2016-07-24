f6cd8ab7d3a882d43714b60d2d1ed576c4a70721
Iansias <maxime.seeleuthner@gmail.com>
2016-07-24 13:25:27 +0200

    jai mis un fichier test

---
+++ Home/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	  <title>Iansias - Indicateur de bon réferencement basique</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+	  	<meta charset="UTF-8">
+		<meta name="description" content="Indicateur Apolitquement  Neutre de Seo Interactif Abitbolesquement Notable à Souhait">
+		<meta name="keywords" content="SEO, Referencement, Abitbol, Bon refenrencement">
+		<meta name="author" content="Iansias: contact:iansias@outlook.fr"> 
+		<meta name="copyright" content="iansias.com">
+		<meta name="identifier-url" content="www.iansias.com ">
+		<meta name="language" content="fr">
+		<html lang="fr">
+		<meta name="robots" content="noindex, nofollow">
+		<meta name="generator" content="Bootply">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--jquery-->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/jquery-2.1.4.min.js"></script>
+
+      <!--bootstrap -->
+      <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <script src="/js/bootstrap.min.js"></script>
+      <link href="css/freelancer.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- fonction circle-->
+    <link href="css/jquery.circliful.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery.circliful.min.js"></script>
+    <!--font awesome-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link href="css/styles.css" rel="stylesheet">
+	<style></style>
+	</head>
+<body>
+
+<div class="Header">
+  <div class="container-fluid" itemscope itemtype="http://schema.org/Product">
+    <div class="row">
+    <div class="col-md-1 logo"><img src="/images/logo_1.png" alt="SEO_Iansias_Logo" class="img-responsive logo">
+    </div>
+      <div class="col-md-9">
+      <h1 itemprop="name">Iansias - Indicateur de bon réferencement basique</h1>
+      <h3 class="lead" itemprop="additionalProperty">Une liste de programmes gratuits vous permettant de savoir rapidement les points à améliorer pour être mieux référencé par Google.</h3>
+    <!-- Ceci est ma maison, sympa non ? c'est français.-->
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid programmes">
+<!--monde de merde -->
+  <div class="row">
+    <div class="col-md-4 col-md-offset-1">
+      <div class="container boxed text-center">
+        <div class="row titre"><h1>Analyseur de Site</h1></div>
+        <div class="row formulairelien">
+          <h3>Analyser votre page web</h3></br>
+          <form action="/rapport/rapport.php" method="POST">
+          URL : <input id="placeholder_url" name="url" size="35" placeholder="http://lesconseilsdelanature.iansias.com"/></br>
+           <input type="submit" class="btn btn-default btn-lg" name="submit" value="Lancer l'analyze"/>
+          </form>
+        </div>
+
+      </div>
+    </div>
+    <div class="col-md-4 col-md-offset-2">
+      <div class="container boxed text-center">
+        <div class="row titre"><h1>Analyser votre chaine youtube</h1></div>
+        <div class="row formulairelien">
+          <h3>Un analyse complet de votre chaine</h3></br>
+          <form action="/rapport/rapport.php" method="POST">
+          URL : <input id="placeholder_url" name="url" size="35" placeholder="http://lesconseilsdelanature.iansias.com"/></br>
+           <input type="submit" class="btn btn-default btn-lg" name="submit" value="Lancer l'analyze"/>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid information">
+      <h1>A propos:</h1></br>
+      <span>Iansias.com est un site qui utilise le langage <strong>PHP</strong> et <strong>Python</strong> pour vous fournir un analyse rapide de votre page internet, votre chaine youtube,</br>
+      et bientôt votre site complet, <strong>gratuitement</strong>, car realisé dans un but purement pédagogique.</br>
+      Le site va évolué dans les mois à venir afin d'être plus complet, vous serez mis-au-courant quand il sera amelioré.</br>
+      de tout façon, si vous êtes là, ça n'est pas par hazard, vu que le site n'est même pas encore référencé.</span>
+
+</div>
+<section id="contacte"></section>
+<?php include_once  "/home/iansiasc/public_html/structur/contactus.php";?>
+
+
+<footer>
+<?php include_once  "/home/iansiasc/public_html/structur/footer.php";?>
+</footer>
+
+
+
+
+
+
+  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67311511-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
