a8c8a41af4f4845637e1e10252408cb187f18393
Maxime Seeleuthner <Maxime Seeleuthner>
2016-07-25 13:46:10 +0200

    test surface

---
+++ Home/accueil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	  <title>Agathe vous gate - Accueil</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+	  	<meta charset="UTF-8">
+		<meta name="description" content="blabla SEO">
+		<meta name="keywords" content="blabla Seo">
+		<meta name="author" content="Iansias: contact:iansias@outlook.fr"> 
+		<meta name="copyright" content="agathevousgate.com">
+		<meta name="identifier-url" content="www.agathevousgate.com">
+		<meta name="language" content="fr">
+		<html lang="fr">
+		<meta name="robots" content="noindex, nofollow">
+		<meta name="generator" content="Bootply">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--jquery-->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/jquery-2.1.4.min.js"></script>
+
+<!--bootstrap -->
+<!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <script src="/js/bootstrap.min.js"></script>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fonction circle-->
+    <!--font awesome-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link href="css/styles.css" rel="stylesheet">
+	<style></style>
+	</head>
+<body>
+</body>
+</html>
