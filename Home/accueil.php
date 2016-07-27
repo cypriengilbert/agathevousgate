a2c693c06351bc55342b001069c7ba30ef037265
Maxime Seeleuthner <Maxime Seeleuthner>
2016-07-28 01:25:05 +0200

    structure de base OK, CSS minimaliste, Responsif comme un coup de tel avec Julie

--- Home/accueil.php
+++ Home/accueil.php
@@ -30,5 +30,187 @@
 	<style></style>
 	</head>
 <body>
+
+<section id="Follow_us"></section>
+<!-- Future place dans le menu-->
+
+
+
+<section id="Header"></section>
+<div class="container header">
+      <div class="row">
+        <div class="col-sm-5 col-xs-3"></div>
+        <div class="col-sm-2 col-xs-6 logo_div"><img src="images/logo_test.jpg" alt="Noeud_papillion_agathevousgate" style="width:100%;height:auto; min-width:150px;"></div>
+        <div class="col-sm-5 col-xs-3"></div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12">
+      	
+      <section id="slider-homepage">
+        <div class="container">
+            <div id="carousel-example-generic" class="carousel slide index-carousel" data-ride="carousel">
+                <!-- Indicators -->
+                <!--<ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                </ol>-->
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <a href="londres.php">
+                            <img src="/agathevousgate/web/img/slider-1.jpg" alt="slider">
+                        </a>
+                        <!--<div class="carousel-caption">
+                            Londres
+                        </div>-->
+                    </div>
+                    <div class="item">
+                        <img src="/agathevousgate/web/img/slider-2.jpg" alt="slider">
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Précédent</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Suivant</span>
+                </a>
+            </div>
+        </div>
+    </section>
+        
+        
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-text-center12">
+        	<h2>Un noeud innovant</h2>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-12">
+        	<p>Le concept de noeuds papillon Agathe Vous Gâte est innovant et personnalisé. Créez vos propres noeuds pap' made in France et Accrocher-les partouts</p>
+        </div>
+      </div>
+
+</div>
+
+
+
+<section id="Cree_noeud"></section>
+<div class="container customisation_noeud">
+      <div class="row">
+        <div class="col-sm-4 col-xs-4"></div>
+        <div class="col-sm-4 col-xs-4"><img src="images/noeud_custom.png" alt="logo noeud" style="width: 100%; height: auto; min-width: 4em;"></div>
+        <div class="col-sm-4 col-xs-4"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-3 col-xs-1"></div>
+        <div class="col-sm-6 col-xs-10"><h2>Créer votre propre noeud</h2></div>
+        <div class="col-sm-3 col-xs-1"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-4 col-xs-1"></div>
+        <div class="col-sm-4 col-xs-10"><a href="#" class="btn btn-primary btn-lg active" role="button">Ok, let's go</a></div>
+        <div class="col-sm-4 col-xs-1"></div>
+      </div>
+</div>
+
+<!-- Generer via base de donnée ? Responsifs ? juste des exemples ?--> 
+<section id="nos_collections"></section>
+
+<div class="container nos_collections text-center">
+      <div class="row">
+        <div class="col-sm-12 col-xs-12">Retrouvez toute nos collections</div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 nos_collections"><img src="images/londre1.jpg" alt="londre_noeud" style="width: 100%; height: auto;"></div>
+        <div class="col-sm-6 nos_collections"><img src="images/londre2.jpg" alt="londre noeud" style="width: 100%; height: auto;"></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 nos_collections"><img src="images/paris2.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+        <div class="col-sm-6 nos_collections"><img src="images/paris1.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 nos_collections"><img src="images/Dakkar1.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+        <div class="col-sm-6 nos_collections"><img src="images/Dakkar2.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+      </div>
+</div>
+
+<section id="produits_populaire"></section>
+<div class="container-fluid produits_populaires">
+      <div class="row text-center">
+        <div class="col-sm-3 col-xs-1"></div>
+        <div class="col-sm-6 col-xs-10"><h3>Produits populaires</h3></div>
+        <div class="col-sm-3 col-xs-1"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-4"><img src="images/image_beta.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+        <div class="col-sm-4"><img src="images/image_beta.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+        <div class="col-sm-4"><img src="images/image_beta.jpg" alt="logo noeud" style="width: 100%; height: auto;"></div>
+      </div>
+</div>
+
+<section id="made_in_france"></section>
+<div class="container made_in_france">
+      <div class="row">
+        <div class="col-sm-4 col-xs-2"></div>
+        <div class="col-sm-4 col-xs-8"><img src="images/madeinfrance" alt="madeinfrance" style="width: 100%; height: auto; min-width: 4em;"></div>
+        <div class="col-sm-4 col-xs-2"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-1 col-xs-1"></div>
+        <div class="col-sm-10 col-xs-10">Les noeuds-papillon Agathe Vous Gâte sont innovants, tendances et personnalisés</div>
+        <div class="col-sm-1 col-xs-1"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-1 col-xs-1"></div>
+        <div class="col-sm-10 col-xs-10">Une invention pratique et facile, fabriquée en France, pour habiller toute la famille !</div>
+        <div class="col-sm-1 col-xs-1"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-1 col-xs-1"></div>
+        <div class="col-sm-10 col-xs-10">Hommes, femmes et enfants composent et accrochent leur noeud comme bon leur semble.</div>
+        <div class="col-sm-1 col-xs-1"></div>
+      </div>
+
+</div>
+
+<section id="Partagez_vos_creations"></section>
+
+<section id="footer"></section>
+<div class="container-fluid footer">
+      <div class="row text-center">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3">Un événement</div>
+        <div class="col-sm-3">Condition générale de vente</div>
+        <div class="col-sm-3">Une question</div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3">Un noeud sur-mesure</div>
+        <div class="col-sm-3">Paiement 100% sécurisé</div>
+        <div class="col-sm-3"></div>
+      </div>
+      <div class="row text-center">
+        <div class="col-sm-3">Agathe vous gâte</div>
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3"></div>
+      </div>
+</div>
+<div class="container-fluid copyright">
+  <div class="row text-center">
+    <div class="col-sm-4 col-xs-1"></div>
+    <div class="col-sm-4 col-xs-10">Copyright 2016 - Agathe Lefeuvre</div>
+    <div class="col-sm-4 col-xs-1"></div>
+  </div>
+</div>
+
 </body>
 </html>
