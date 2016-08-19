d4ffb255e0591ed8241b3476f764259d9524ef7e
Maxime Seeleuthner <Maxime Seeleuthner>
2016-08-19 18:16:11 +0200

    beta test noeud

--- src/CommerceBundle/Form/CollectionType.php
+++ src/CommerceBundle/Form/CollectionType.php
@@ -5,7 +5,7 @@ namespace CommerceBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+/*use Symfony\Component\Form\Extension\Core\Type\CollectionType;*/
 use CommerceBundle\Form\ColorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
